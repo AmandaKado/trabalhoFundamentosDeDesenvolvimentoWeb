cec11bcd4559f1e4d918b2db3bcabe4616ff4f63
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2025-07-03 17:52:53 -0300

    Menu concluido
    Acrescentado elemento crescer no hover

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -6,7 +6,6 @@ export function iniciarMenu() {
     const linksDoMenu = document.querySelectorAll('.itemNavegacao');
 
     const fecharSeClicarFora = (event) => {
-
         if (!nav.contains(event.target) && event.target !== menuHamburguer) {
             fecharMenuAberto(); 
         }
@@ -25,9 +24,38 @@ export function iniciarMenu() {
     if (menuHamburguer && fecharMenu && nav) {
         menuHamburguer.addEventListener('click', abrirMenu);
         fecharMenu.addEventListener('click', fecharMenuAberto); 
-    
         linksDoMenu.forEach(link => {
             link.addEventListener('click', fecharMenuAberto);
         });
     }
+
+    const secoes = document.querySelectorAll('section[id]');
+
+    const atualizarMenuAtivo = () => {
+
+        const checkpoint = window.scrollY + (window.innerHeight / 2);
+
+        secoes.forEach(secao => {
+            const topoSecao = secao.offsetTop;
+            const alturaSecao = secao.offsetHeight;
+            const idSecao = secao.getAttribute('id');
+
+
+            const checkpointEstaDentroDaSecao = checkpoint >= topoSecao && checkpoint <= topoSecao + alturaSecao;
+
+            if (checkpointEstaDentroDaSecao) {
+
+                const linkAtivo = document.querySelector(`.itemNavegacao[href*=${idSecao}]`);
+
+                linksDoMenu.forEach(link => link.parentElement.classList.remove('ativo'));
+
+                if (linkAtivo) {
+                    linkAtivo.parentElement.classList.add('ativo');
+                }
+            }
+        });
+    };
+
+    window.addEventListener('scroll', atualizarMenuAtivo);
+
 };
