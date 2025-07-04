8f72c8407a1f719e3dbe690a95e634b7aaaff9d3
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2025-07-03 22:47:37 -0300

    Tirando efeito

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 
 import { iniciarMenu } from '../js/modules/menu.js'; 
 import { iniciarDigitacao } from '../js/modules/digitacao.js';
-import { iniciarEfeitoImagem } from '../js/modules/efeitoImagem.js';
 
 iniciarMenu();
 iniciarDigitacao();
-iniciarEfeitoImagem();
 
