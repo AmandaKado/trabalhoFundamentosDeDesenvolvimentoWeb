4deef1ec04640d43745123b186adb6809740e488
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2025-07-03 22:37:17 -0300

    arrumando semantica

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Trabalho de Ferramentas de Desenvolvimento Web
 
-Desenvolvido somente com HTML, CSS E JavasCript! 
+Desenvolvido somente com HTML, CSS E JavasCript! Feito em Mobile First
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 import { iniciarMenu } from '../js/modules/menu.js'; 
 import { iniciarDigitacao } from '../js/modules/digitacao.js';
+import { iniciarEfeitoImagem } from '../js/modules/efeitoImagem.js';
 
 iniciarMenu();
 iniciarDigitacao();
+iniciarEfeitoImagem();
 
