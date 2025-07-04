3b3de86ae00e715867a6ea5a956ec9ab2a56873e
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2025-07-03 23:09:56 -0300

    fadeInSite

---
+++ js/modules/scroll.js
@@ -0,0 +1,23 @@
+export function iniciarAnimacaoScroll() {
+    function iniciarAnimacaoScroll() {
+        const elementos = document.querySelectorAll('.fadeInElemento');
+        if (elementos.length === 0) return;
+
+        const observer = new IntersectionObserver((entradas) => {
+            entradas.forEach(entrada => {
+                if (entrada.isIntersecting) {
+                    entrada.target.classList.add('visivel');
+                }
+            });
+        }, { threshold: 0.2 });
+
+        elementos.forEach(el => observer.observe(el));
+    }
+
+    // Garanta que ela é chamada no final
+    document.addEventListener('DOMContentLoaded', () => {
+        // ...
+        iniciarAnimacaoScroll();
+        // ...
+    });
+};
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 import { iniciarMenu } from '../js/modules/menu.js'; 
 import { iniciarDigitacao } from '../js/modules/digitacao.js';
+import { iniciarAnimacaoScroll } from '../js/modules/scroll.js';
 
 iniciarMenu();
 iniciarDigitacao();
+iniciarAnimacaoScroll();
 
